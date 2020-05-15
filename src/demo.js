033c08e65582cba5bdaeaa371f2abaeca2f4cf2b
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-05-15 14:33:45 -0300

    Load code

--- src/demo.js
+++ src/demo.js
@@ -6,6 +6,10 @@ import Demo0 from "../guide/0.0.class";
 import Demo1 from "../guide/0.1.class";
 import Demo2 from "../guide/0.2.class";
 import { SmoothView } from "./smooth-view";
+import { MiniEditor } from "./mini-editor/mini-editor";
+import code0 from "!!raw-loader!../guide/0.0.class.js";
+import code1 from "!!raw-loader!../guide/0.1.class.js";
+import code2 from "!!raw-loader!../guide/0.2.class.js";
 
 export function Demo() {
   const [state, setState] = React.useState({
@@ -15,59 +19,88 @@ export function Demo() {
   });
   const playerRef = React.useRef();
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{}}>
-        <MiniBrowser url="http://localhost:3000/" height={300}>
-          <div className="demo-container">
-            <SmoothView stepIndex={state.currentIndex}>
-              <Demo0 name={`Marty`} />
-              <Demo1 />
-              <Demo2 />
-            </SmoothView>
-          </div>
-        </MiniBrowser>
-        <div
-          style={{
-            position: "relative",
-            borderRadius: "50%",
-            overflow: "hidden",
-            height: 200,
-            width: 200,
-            margin: "15px auto",
-          }}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        height: "calc(100vh - 16px)",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: 600,
+          width: "100%",
+        }}
+      >
+        <MiniEditor
+          files={["index.js"]}
+          active="index.js"
+          style={{ width: 500 }}
         >
-          <VideoPlayer
-            ref={playerRef}
-            steps={steps}
+          <SmoothView stepIndex={state.currentIndex}>
+            <pre>{code0}</pre>
+            <pre>{code1}</pre>
+            <pre>{code2}</pre>
+          </SmoothView>
+        </MiniEditor>
+        <div style={{ width: 40 }} />
+        <div style={{}}>
+          <MiniBrowser url="http://localhost:3000/" height={400}>
+            <div className="demo-container">
+              <SmoothView stepIndex={state.currentIndex}>
+                <Demo0 name={`Marty`} />
+                <Demo1 />
+                <Demo2 />
+              </SmoothView>
+            </div>
+          </MiniBrowser>
+          <div
             style={{
-              position: "absolute",
-              transform: "translateX(-30px)",
+              position: "relative",
+              borderRadius: "50%",
+              overflow: "hidden",
+              height: 185,
+              width: 185,
+              margin: "15px auto",
             }}
-            onChange={({ stepIndex, stepProgress }) =>
-              setState((s) => ({ ...s, currentIndex: stepIndex, stepProgress }))
-            }
-          />
+          >
+            <VideoPlayer
+              ref={playerRef}
+              steps={steps}
+              style={{
+                position: "absolute",
+                transform: "translateX(-30px)",
+              }}
+              onChange={({ stepIndex, stepProgress }) =>
+                setState((s) => ({
+                  ...s,
+                  currentIndex: stepIndex,
+                  stepProgress,
+                }))
+              }
+            />
+          </div>
         </div>
-        {state.currentIndex} - {state.stepProgress}
+        <Slider
+          inputSteps={steps}
+          currentIndex={state.currentIndex}
+          stepProgress={state.stepProgress}
+          isPlaying={state.isPlaying}
+          onChange={({ stepIndex, stepProgress }) =>
+            playerRef.current.seek(stepIndex, stepProgress, false)
+          }
+          play={() => {
+            playerRef.current.play();
+            setState((s) => ({ ...s, isPlaying: true }));
+          }}
+          pause={() => {
+            playerRef.current.pause();
+            setState((s) => ({ ...s, isPlaying: false }));
+          }}
+        />
       </div>
-      <div>{state.currentIndex}</div>
-      <Slider
-        inputSteps={steps}
-        currentIndex={state.currentIndex}
-        stepProgress={state.stepProgress}
-        isPlaying={state.isPlaying}
-        onChange={({ stepIndex, stepProgress }) =>
-          playerRef.current.seek(stepIndex, stepProgress, false)
-        }
-        play={() => {
-          playerRef.current.play();
-          setState((s) => ({ ...s, isPlaying: true }));
-        }}
-        pause={() => {
-          playerRef.current.pause();
-          setState((s) => ({ ...s, isPlaying: false }));
-        }}
-      />
     </div>
   );
 }
