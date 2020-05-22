569745e776b910adfc3ea529b1720363698ae01a
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-05-22 13:55:01 -0300

    Add second chapter

--- src/steps.js
+++ src/steps.js
@@ -187,7 +187,7 @@ const steps = [
     },
   },
   {
-    player: { ...t("19:59-20:20") },
+    player: { ...t("19:59-20:12") },
     demo: <Demo1 />,
     editor: {
       code: require("!!raw-loader!../guide/0.11.js").default,
@@ -197,7 +197,35 @@ const steps = [
       tabs: ["ClassExample.js"],
     },
   },
+  chapter1("20:12-20:28", "1.0.js", <Demo0 name={`Mary`} />),
+  chapter1("20:28-20:31", "1.1.js", <Demo0 name={`Mary`} />),
+  chapter1("20:31-20:42", "1.2.js", <Demo0 name={`Mary`} />),
+  chapter1("20:42-20:51", "1.3.js", <Demo0 name={`Mary`} />),
+  chapter1("20:51-20:58", "1.4.js", <Demo0 name={`Mary`} />),
+  chapter1("20:58-21:13", "1.5.js", <Demo0 name={`Mary`} />),
+  chapter1("21:13-21:20", "1.6.js", <Demo0 name={`Mary`} />),
+  chapter1("21:20-21:22", "1.7.js", <Demo0 name={`Mary`} />),
+  chapter1("21:31-21:48", "1.7.js", <Demo0 name={`Mary`} />, { focus: "5,6" }),
+  chapter1("21:48-22:10", "1.8.js", <Demo0 name={`Mary`} />),
+  chapter1("22:10-22:16", "1.9.js", <Demo0 name={`Mary`} />),
+  chapter1("22:18-22:24", "1.10.js", <Demo0 name={`Mary`} />, { focus: "1,5" }),
+  chapter1("22:24-22:26", "1.10.js", <Demo0 name={`Mary`} />),
+  chapter1("22:26-22:39", "1.10.js", <Demo1 />),
 ];
+
+function chapter1(time, file, demo, { focus } = {}) {
+  return {
+    player: t(time),
+    demo,
+    editor: {
+      code: require(`!!raw-loader!../guide/${file}`).default,
+      lang: "jsx",
+      focus,
+      file: "FunctionExample.js",
+      tabs: ["ClassExample.js", "FunctionExample.js"],
+    },
+  };
+}
 
 function t(ts) {
   const [startString, endString] = ts.split("-");
